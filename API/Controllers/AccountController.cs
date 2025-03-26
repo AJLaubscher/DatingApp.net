6050d90fe9fd358353c7a4f318557e439baca49f
Andre Laubscher <andre@bitcube.tech>
2025-03-26 19:51:50 +0200

    Login endpoint + DTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
@@ -33,6 +34,28 @@ namespace API.Controllers
 
             dbContext.Users.Add(user);
             await dbContext.SaveChangesAsync();        
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await dbContext.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if(user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedhash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedhash.Length; i++)
+            {
+                if(computedhash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
 
             return user;
         }
