4a56b62aa243da2be7d13a38ebb98e154707b578
Andre Laubscher <andre@bitcube.tech>
2025-04-05 15:42:34 +0200

    Use automapper queryable

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,12 +1,31 @@
 using System;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class UserRepository(DataContext dbContext) : IUserRepository
+public class UserRepository(DataContext dbContext, IMapper mapper) : IUserRepository
 {
+
+    public async Task<MemberDto?> GetMemberAsync(string username)
+    {
+        return await dbContext.Users
+            .Where(x => x.UserName == username)
+            .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    {
+        return await dbContext.Users
+            .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+            .ToListAsync();
+    }
+
     public async Task<AppUser?> GetUserByIdAsync(int id)
     {
         return await dbContext.Users.FindAsync(id);
