1bbcd5e8437f046c89bb18c064762ec35c269884
Andre Laubscher <andre@bitcube.tech>
2025-04-06 14:15:21 +0200

    persisting changes in the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+using System.Net.Mail;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -12,7 +14,7 @@ using Microsoft.OpenApi.Validations;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
 
         [HttpGet]
@@ -36,5 +38,25 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MembersUpdateDto membersUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(username == null)
+            {
+                return BadRequest("No username found in token");
+            }
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if(user == null) return BadRequest("Could not find user");
+
+            mapper.Map(membersUpdateDto, user);
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
+        }
     }
 }
