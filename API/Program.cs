d737e96c7b950c77d1b09b6d3d3098ab22c5ee67
Andre Laubscher <andre@bitcube.tech>
2025-05-01 15:25:40 +0200

    Section 17

--- API/Program.cs
+++ API/Program.cs
@@ -44,6 +44,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await dbContext.Database.MigrateAsync();
+    await dbContext.Database.ExecuteSqlRawAsync("DELETE FROM Connections");
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
