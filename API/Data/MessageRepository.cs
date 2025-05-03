6fb45486e4f9da8561edbc8c8b7e9ec772d377c6
Andre Laubscher <andre@bitcube.tech>
2025-05-03 10:57:08 +0200

    Section 18 (unit of work)

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -93,7 +93,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
         if (unreadMessages.Count != 0)
         {
             unreadMessages.ForEach(x => x.DateRead = DateTime.UtcNow);
-            await context.SaveChangesAsync();
+
         }
 
         return messages;
@@ -104,8 +104,5 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
         context.Connections.Remove(connection);
     }
 
-    public async Task<bool> SaveAllAsync()
-    {
-        return await context.SaveChangesAsync() > 0;
-    }
+
 }
